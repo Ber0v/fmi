12650387489d74d4d80566b27342a1667d22f771
Ber0v <AlexBerov123@gmail.com>
2026-01-10 21:27:18 +0200

    zad

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
 
-int myAtoi(const char* str)
-{
-	int i = 0;
-	int result = 0;
-	int sign = 1;
-
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-
-	return result * sign;
-}
-
 int main()
 {
-	char text[100];
-	std::cin.getline(text, 100);
 
-	std::cout << myAtoi(text);
-	return 0;
 }
